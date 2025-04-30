f49d126e6674c0c6e7578524b9df782b5b99155c
Revathi S <trulyrev1@gmail.com>
2025-04-30 18:57:52 +0530

    commit

---
+++ client/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h2>Login Component</h2>
+      <p>This is a placeholder login screen.</p>
+    </div>
+  );
+};
+
+export default Login;
